a2f104b1535800a29c93c33ed6e2f13483987220
Daniil Simonyan <danila718@gmail.com>
2022-08-29 17:04:45 +0300

    установка MongoDB и nestjf/mongoose

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,29 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = async (
+  configService: ConfigService,
+): Promise<MongooseModuleOptions> => {
+  return {
+    uri: getMongoString(configService),
+    // ...getMongoOptions(),
+  };
+};
+
+export const getMongoString = (configService: ConfigService): string =>
+  'mongodb://' +
+  configService.get('MONGO_LOGIN') +
+  ':' +
+  configService.get('MONGO_PASSWORD') +
+  '@' +
+  configService.get('MONGO_HOST') +
+  ':' +
+  configService.get('MONGO_PORT') +
+  '/' +
+  configService.get('MONGO_AUTHDATABASE');
+
+// export const getMongoOptions = (): MongooseModuleOptions => ({
+// useNewUrlParser: true,
+// useCreateIndex: true,
+// useUnifiedTopology: true,
+// });
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,19 +1,14 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
-  constructor(private readonly configService: ConfigService) {}
-
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>): Promise<void> {}
 
   @Get(':id')
-  async get(@Param('id') id: string): Promise<void> {
-    this.configService.get('TEST');
-  }
+  async get(@Param('id') id: string): Promise<void> {}
 
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {}
