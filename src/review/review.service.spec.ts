57ff64b64719d2ff4046bb2de0e9dbc644ce65ab
Daniil Simonyan <danila718@gmail.com>
2022-08-30 14:43:51 +0300

    unit test for review findByProject

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,24 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { ReviewService } from './review.service';
+import { Review } from './schemas/review.schema';
 
 describe('ReviewService', () => {
   let service: ReviewService;
 
+  const exec = { exec: jest.fn() };
+
+  const reviewRepositoryFactory = () => ({
+    find: () => exec,
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
+      providers: [
+        ReviewService,
+        { useFactory: reviewRepositoryFactory, provide: getModelToken(Review.name) },
+      ],
     }).compile();
 
     service = module.get<ReviewService>(ReviewService);
@@ -14,5 +26,14 @@ describe('ReviewService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByProductId - success', async () => {
+    const id = new Types.ObjectId().toHexString();
+    reviewRepositoryFactory()
+      .find()
+      .exec.mockReturnValueOnce([{ product: id }]);
+    const res = await service.findByProductId(id);
+    expect(res[0].product).toBe(id);
   });
 });
