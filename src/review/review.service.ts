e2db0753f9953c5c3e7757fb990127ce2e8104cb
Daniil Simonyan <danila718@gmail.com>
2022-08-30 10:09:05 +0300

    Add method deleteByProductId in ReviewService

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { DeleteResult } from 'mongodb';
 import { Model, Types } from 'mongoose';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { Review, ReviewDocument } from './schemas/review.schema';
@@ -19,5 +20,10 @@ export class ReviewService {
 
   async findByProductId(productId: string): Promise<Review[]> {
     return this.review.find({ product: new Types.ObjectId(productId) }).exec();
+  }
+
+  async deleteByProductId(productId: string): Promise<number> {
+    const result = await this.review.deleteMany({ product: new Types.ObjectId(productId) }).exec();
+    return result.deletedCount ?? 0;
   }
 }
